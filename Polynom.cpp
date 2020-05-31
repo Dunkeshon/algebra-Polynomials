28266d01355c2e62584f349b89c5a76ecb479f9d
darinamazur <allottcentr@gmail.com>
2020-05-31 09:49:17 +0300

    remove references

--- Polynom.cpp
+++ Polynom.cpp
@@ -549,7 +549,7 @@ void Polynom::quot_rem(Polynom& A, Polynom& B, Polynom& Q, Polynom& R) {
 }
 
 
-Polynom& Polynom::gcd(Polynom& a, Polynom& b) {
+Polynom Polynom::gcd(Polynom& a, Polynom& b) {
     if (b.isZero()) {
         return a;
     }
@@ -581,7 +581,7 @@ Polynom& Polynom::operator=(Polynom& other)
 }
 
 
-Polynom& GCD(Polynom a, Polynom b) {
+Polynom GCD(Polynom a, Polynom b) {
     Polynom::handleException(a, b);
 
     if (a.power < b.power) {
